b420982a4f13b004b35ee2a642db43796d13f12a
Connor <cmg58665@gmail.com>
2019-09-28 16:25:29 -0500

    3

--- forms/LoanCalculator/LoanCalculator.js
+++ forms/LoanCalculator/LoanCalculator.js
@@ -1,38 +1,49 @@
-/*Requirements: 
-
-a function named homeLoanPayment that takes 3 arguments and returns the monthly payment to the main program.  Term can only be 15 or 30 years.
-main program that asks the user which calculator they want to use, gets their data and calls the function.
-
-main program that gets the answer back from the function and outputs it to an alert in this format: 
-     A home loan for $200,000 over 30 years at 5.7% interest would have a monthly payment of $1160.80.
-OR
-     A car loan for $30,000 over 60 months at 3.2% interest would have a monthly payment of $542.00
-The program must keep running until the user says they want to stop running it. 
-Hint: a loop would be handy here..../* 
-
-
-//Car Loan Payment 
-
-*/
-
-
-
-  
-
 function carLoanPayment(amount, interestRate, months) {
   interestRate = (interestRate/ 100) / 12
   payment = amount * (interestRate*(Math.pow(1+interestRate,months)) / (Math.pow(1+interestRate, months) -1 ))
   return (payment.toFixed(2))
  
   }
+  
+  
+function homeLoanPayment(amount, interestRate, years) {
+  interestRate = (interestRate/ 100) / 12
+  months = years * 12
+  payment = amount * (interestRate*(Math.pow(1+interestRate,months)) / (Math.pow(1+interestRate, months) -1 ))
+  return payment.toFixed(2)
+  }
+///////////////////////////////////////////////////////////////////////////////  
 
 
-let input1 = prompt('Which calculator would you like to use: Car or Home');
+let input1 = prompt('Which calculator would you like to use: Car or Home. Enter Stop to quit.');
 
-if (input1 == 'Car'){
-  loan = prompt('Please enter the loan amount.');
-  rate = prompt('Please enter the interest rate.');
-  term = prompt('Please enter the term in months.');
-  alert(`A car loan for $${loan} over ${term} months at ${rate}% interest would have a monthly payment of $${carLoanPayment(loan, rate, term)}.`);
 
+while (input1 != 'Stop') {
+  
+ if (input1 == 'Car'){
+    loan = prompt('Please enter the loan amount.');
+    rate = prompt('Please enter the interest rate.');
+    term = prompt('Please enter the term in months.');
+    alert(`A car loan for $${loan} over ${term} months at ${rate}% interest would have a monthly payment of $${carLoanPayment(loan, rate, term)}.`);
+
+  } else if (input1 == 'Home') {
+    loan = prompt('Please enter the loan amount.');
+    rate = prompt('Please enter the interest rate.');
+    term = prompt('Please enter the term in years.');
+    if (term == 15 || term == 30) {
+      alert(`A home loan for $${loan} over ${term} years at ${rate}% interest would have a monthly payment of $${homeLoanPayment(loan, rate, term)}.`);
+    }
+    while (term != 15 || term != 30) {
+      alert('The loan term can only be 15 or 30 years');
+      term = prompt('Please enter the term in years.');
+      if (term == 15 || term == 30){
+       alert(`A home loan for $${loan} over ${term} years at ${rate}% interest would have a monthly payment of $${homeLoanPayment(loan, rate, term)}.`);
+       break;
+      }
+    }
+    
+  } else {
+    alert('error');
+  }
+  input1 = prompt('Which calculator would you like to use: Car or Home. Enter Stop to quit.');
 }
